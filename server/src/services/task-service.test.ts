a7325f762dbcd7c5a4042e31a9eab4cc7505ad9a
Piyush Ramavat <7731676+piyush-ramavat@users.noreply.github.com>
2024-09-02 14:01:25 +1000

    mock data added

--- server/src/services/task-service.test.ts
+++ server/src/services/task-service.test.ts
@@ -1,6 +1,8 @@
-import { mockDeep } from "jest-mock-extended";
+import { DeepMockProxy, mockDeep } from "jest-mock-extended";
 import { PrismaClient as OriginalPrismaClient } from "@prisma/client";
 import { createTask } from "./task-services";
+import { dbService } from "../lib/db-service";
+import { CreateTaskRequest } from "../lib/types";
 
 jest.mock("@prisma/client", () => ({
   PrismaClient: function () {
@@ -8,15 +10,22 @@ jest.mock("@prisma/client", () => ({
   },
 }));
 
+const db = dbService();
+const prismaMock = db as unknown as DeepMockProxy<OriginalPrismaClient>;
+
 describe("Task Services", () => {
   it("creates a task", async () => {
-    const result = await createTask({
+    const task: CreateTaskRequest = {
       name: "Test Task",
       description: "Test description",
       dueDate: new Date(),
       userId: 0,
-    });
+    };
+    prismaMock.task.create.mockResolvedValue({ ...task, id: 123, createdAt: new Date(), updatedAt: new Date() });
+
+    const result = await createTask(task);
 
     expect(result).not.toBeNull();
+    expect(result).toHaveProperty("id");
   });
 });
